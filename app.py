abd4eecb9ffdac96c25a0980062ad2e20fdd7a34
Jake Zaia <jzaia@stuy.edu>
2017-11-11 16:57:13 -0500

    Temp push to continue work on local machine

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
-import urllib2 as u
+import urllib2
+import random
 from json import loads
 
 app = Flask(__name__)
-api_url = "https://api.nasa.gov/planetary/apod?api_key=tbTCi4JbCHJ7zrVjTb9Na7ksIJPkTxPoymdAoBJi"
+api_url = "http://pokeapi.co/api/v2/pokemon/"
 
 @app.route('/')
 def root():
-    json = u.urlopen(api_url).read()
+    url = api_url + str(int(random.random() *802 + 1))
+    print url
+    req = urllib2.Request(url)
+    #urllib2.Request.add_header('User-agent', 'Please work')
+    json = urllib2.urlopen(url)
+    json = json.read()
     data = loads(json)
+    print "HOI!!!!"
     return render_template('index.html', data = data)
 
 if __name__ == '__main__':
